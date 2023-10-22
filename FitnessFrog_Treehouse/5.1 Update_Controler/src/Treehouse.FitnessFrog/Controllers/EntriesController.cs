1b338c3955c8316951d511f1881a674b497db52a
tanmoysr <tanmoysr2026@yahoo.com>
2023-10-22 16:23:55 -0400

    Upload_V1

---
+++ FitnessFrog_Treehouse/5.1 Update_Controler/src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Treehouse.FitnessFrog.Data;
+using Treehouse.FitnessFrog.Models;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private EntriesRepository _entriesRepository = null;
+
+        public EntriesController()
+        {
+            _entriesRepository = new EntriesRepository();
+        }
+
+        public ActionResult Index()
+        {
+            List<Entry> entries = _entriesRepository.GetEntries();
+
+            // Calculate the total activity.
+            double totalActivity = entries
+                .Where(e => e.Exclude == false)
+                .Sum(e => e.Duration);
+
+            // Determine the number of days that have entries.
+            int numberOfActiveDays = entries
+                .Select(e => e.Date)
+                .Distinct()
+                .Count();
+
+            ViewBag.TotalActivity = totalActivity;
+            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+            return View(entries);
+        }
+
+        public ActionResult Add()
+        {
+            var entry = new Entry()
+            {
+                Date = DateTime.Today,
+            };
+
+            ViewBag.ActivitiesSelectListItems = new SelectList(
+                Data.Data.Activities, "Id", "Name");
+
+            return View(entry);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Entry entry)
+        {
+            // If there aren't any "Duration" field validation errors
+            // then make sure that the duration is greater than "0".
+            if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
+            {
+                ModelState.AddModelError("Duration", 
+                    "The Duration field value must be greater than '0'.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ActivitiesSelectListItems = new SelectList(
+                Data.Data.Activities, "Id", "Name");
+
+            return View(entry);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FitnessFrog
-This is a project for ASP.NET MVC Forms. I followed the course "ASP.NET MVC Forms" by James Churchill from Treehouse. It contains two folders. To see the final project look into the folder "FitnessFrog_DIY". For step by step process look into the folder "FitnessFrog_Treehouse".
+This is a project for ASP.NET MVC Forms. \
+I followed the course "ASP.NET MVC Forms" by James Churchill from Treehouse.\
+It contains two folders. 
+1. To see the final project look into the folder "FitnessFrog_DIY". 
+2. For step by step process look into the folder "FitnessFrog_Treehouse".
+
+## Page Demo
+### Home Page
+![Home](./PageImage/Home.PNG)
+
+### Add Page
+![Home](./PageImage/Add.PNG)
